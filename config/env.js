08fc1cfbc61df5fc8536f7056a1c087df9602079
痕迹 <15035291867@163.com>
2021-08-13 10:29:05 +0800

    chore: 开启热更新

--- config/env.js
+++ config/env.js
@@ -89,7 +89,9 @@ function getClientEnvironment(publicUrl) {
         // react-refresh is not 100% stable at this time,
         // which is why it's disabled by default.
         // It is defined here so it is available in the webpackHotDevClient.
-        FAST_REFRESH: process.env.FAST_REFRESH !== 'false',
+        // 开启热更新
+        FAST_REFRESH: false,
+        // FAST_REFRESH: process.env.FAST_REFRESH !== 'false',
       }
     );
   // Stringify all values so we can feed into webpack DefinePlugin
