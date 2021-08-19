dbc6d331901bfe5d0e74cda43fc3354dd17b1907
痕迹 <15035291867@163.com>
2021-08-19 18:36:01 +0800

    feat: 更新字体库，初步书写login页面

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputItem } from "antd-mobile";
 
 import styles from "./index.module.less";
 
 function Login() {
-  return <div className={styles.login_page}></div>;
+  const [phone, setPhone] = useState("");
+  const onChangePhone = (val) => {
+    setPhone(val);
+  };
+  return (
+    <div className={styles.login_page}>
+      <InputItem
+        type="phone"
+        value={phone}
+        onChange={(val) => {
+          onChangePhone(val);
+        }}
+      ></InputItem>
+    </div>
+  );
 }
 
 export default Login;
