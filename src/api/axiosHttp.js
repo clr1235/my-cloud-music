5814fe4bd7fd86b87f867d3bd6759398391120cb
痕迹 <15035291867@163.com>
2021-08-20 17:21:39 +0800

    feat: 封装axios响应的统一错误处理

--- src/api/axiosHttp.js
+++ src/api/axiosHttp.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Toast } from "antd-mobile";
 
 export function handledGetParams(params) {
   let arr = [];
@@ -17,30 +18,6 @@ export function handledGetParams(params) {
   }
   return `?${arr.join("&")}`;
 }
-
-// 添加请求拦截器
-axios.interceptors.request.use(
-  function (config) {
-    // 在发送请求之前做些什么
-    return config;
-  },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  }
-);
-
-// 添加响应拦截器
-axios.interceptors.response.use(
-  function (response) {
-    // 对响应数据做点什么
-    return response;
-  },
-  function (error) {
-    // 对响应错误做点什么
-    return Promise.reject(error);
-  }
-);
 
 // 处理baseURL
 function getBaseURL(env) {
@@ -62,6 +39,35 @@ function axiosHttp(axiosConfig) {
     timeout: 30000,
     ...axiosConfig,
   });
+  // 添加请求拦截器
+  service.interceptors.request.use(
+    function (config) {
+      // 在发送请求之前做些什么
+      return config;
+    },
+    function (error) {
+      // 对请求错误做些什么
+      return Promise.reject(error);
+    }
+  );
+
+  // 添加响应拦截器
+  service.interceptors.response.use(
+    function (response) {
+      // 对响应数据做点什么
+      if (response.data.code !== 200) {
+        Toast.fail(response.data.message, 3);
+      }
+      return response;
+    },
+    function (error) {
+      // 统一错误处理
+      if (error && error.response) {
+        Toast.fail(error.response.data.message, 3);
+      }
+      return Promise.resolve(error.response.data);
+    }
+  );
   return service(axiosConfig);
 }
 
